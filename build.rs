b1d878ca681e65b8ea188228a28b3d99de6a72db
Grzegorz Nosek <root@localdomain.pl>
2023-11-18 12:19:10 +0100

    Bindgen-based autogenerated bindings

--- build.rs
+++ build.rs
@@ -7,4 +7,17 @@ fn main() {
     println!("cargo:rustc-link-search=native={}/lib", snappy.display());
     println!("cargo:rustc-link-lib=static=snappy");
     println!("cargo:rustc-link-lib=stdc++");
+
+    let snappy_h = snappy.join("include/snappy-c.h");
+    let snappy_h = snappy_h.to_str().unwrap();
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let out_path = out_path.join("bindings.rs");
+
+    bindgen::Builder::default()
+        .header(snappy_h)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(out_path)
+        .expect("Couldn't write bindings!");
 }
---
+++ src/ffi.rs
@@ -0,0 +1,4 @@
+#![allow(non_camel_case_types)]
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
