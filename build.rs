8105175342efee3237f4671b85d39731972f51f1
Grzegorz Nosek <root@localdomain.pl>
2023-11-18 12:46:56 +0100

    Skeleton for the simple cxx.rs interface

--- build.rs
+++ build.rs
@@ -6,5 +6,13 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={}/lib", snappy.display());
     println!("cargo:rustc-link-lib=static=snappy");
-    println!("cargo:rustc-link-lib=stdc++");
+
+    let cxx_bridge_files = vec!["src/lib.rs"];
+
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    let include_dir = std::path::Path::new(&out_dir).join("include");
+    cxx_build::bridges(cxx_bridge_files)
+        .include(include_dir)
+        .flag_if_supported("-fno-rtti")
+        .compile("snappy-cxx-rs");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,46 @@
+#[cxx::bridge(namespace = "snappy")]
+mod ffi {
+    // ...
+}
 
+pub fn compress(input: &[u8]) -> Vec<u8> {
+    cxx::let_cxx_string!(output = "");
+
+    unsafe {
+        ffi::Compress(
+            input.as_ptr() as *const _,
+            input.len(),
+            output.as_mut().get_unchecked_mut() as *mut _,
+        );
+    }
+    output.as_bytes().to_vec()
+}
+
+pub fn uncompress(input: &[u8]) -> Option<Vec<u8>> {
+    cxx::let_cxx_string!(output = "");
+
+    unsafe {
+        if ffi::Uncompress(
+            input.as_ptr() as *const _,
+            input.len(),
+            output.as_mut().get_unchecked_mut() as *mut _,
+        ) {
+            Some(output.as_bytes().to_vec())
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_roundtrip_simple() {
+        let input = b"Hello world!";
+        let compressed = compress(input);
+        let uncompressed = uncompress(&compressed).unwrap();
+        assert_eq!(input, &uncompressed[..]);
+    }
+}
